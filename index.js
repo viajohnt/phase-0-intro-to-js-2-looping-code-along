83215960954de05d3bbf1b7467b1247cd884b621
viajohnt <118217497+viajohnt@users.noreply.github.com>
2022-12-30 16:22:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function writeCards(names, gift) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${gift} gift!`);
+  }
+  
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
