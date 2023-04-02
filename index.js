8b6a1234890d9743803f4fed797176f25cb23b02
John Truong <viajohnt@gmail.com>
2023-04-01 21:39:51 -0600

    Final Commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-function writeCards(names, gift) {
-    return names.map(name => `Thank you, ${name}, for the wonderful ${gift} gift!`);
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
   }
-  
-  function countDown(num) {
-    for (let i = num; i >= 0; i--) {
-      console.log(i);
-    }
+  return thankYouCards
+}
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
   }
+  console.log( startingNumber );
+}
